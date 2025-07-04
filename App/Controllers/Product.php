43a9d72d89eda69a0a2d032b8b54579e4fb34927
GuillainThomas <thomas.guillain@viacesi.fr>
2025-07-04 11:31:22 +0200

    fix: issue#1: block the form submission if there is no image uploaded and display an precise error message

--- App/Controllers/Product.php
+++ App/Controllers/Product.php
@@ -19,9 +19,15 @@ class Product extends \Core\Controller
     public function indexAction()
     {
 
+        $error = null;
+
         if(isset($_POST['submit'])) {
 
             try {
+                if (!isset($_FILES['picture']) || $_FILES['picture']['error'] === UPLOAD_ERR_NO_FILE) {
+                    throw new \Exception('Une image est requise');
+                }
+
                 $f = $_POST;
 
                 // TODO: Validation
@@ -29,17 +35,24 @@ class Product extends \Core\Controller
                 $f['user_id'] = $_SESSION['user']['id'];
                 $id = Articles::save($f);
 
-                $pictureName = Upload::uploadFile($_FILES['picture'], $id);
-
-                Articles::attachPicture($id, $pictureName);
+                try {
+                    $pictureName = Upload::uploadFile($_FILES['picture'], $id);
+                    Articles::attachPicture($id, $pictureName);
+                } catch (\Exception $e) {
+                    Articles::delete($id);
+                    throw $e;
+                }
 
                 header('Location: /product/' . $id);
+                return;
             } catch (\Exception $e){
-                    var_dump($e);
+                $error = $e->getMessage();
             }
         }
 
-        View::renderTemplate('Product/Add.html');
+        View::renderTemplate('Product/Add.html', [
+            'error' => $error
+        ]);
     }
 
     /**
