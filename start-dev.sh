4c0d6d6052ec8b2474e5e4621ea70706b9cb7234
GuillainThomas <thomas.guillain@viacesi.fr>
2025-07-04 10:29:22 +0200

    feat: add swagger api documentation

--- start-dev.sh
+++ start-dev.sh
@@ -2,8 +2,6 @@
 
 echo "🚀 Démarrage de l'environnement DEV..."
 
-docker-compose --env-file .env.dev -p vide-grenier-dev up -d --build
-# docker-compose --env-file .env.dev -f docker-compose.yml -f docker-compose.dev.yml -p vide-grenier-dev up -d --build
-# docker-compose --env-file .env.dev -f docker-compose.dev.yml -p videtoncube-dev up -d 
+docker-compose --env-file .env.dev -f docker-compose.yml -f docker-compose.dev.yml -p vide-grenier-dev up -d --build
 
 echo "✅ Environnement DEV démarré sur http://localhost:9000"
